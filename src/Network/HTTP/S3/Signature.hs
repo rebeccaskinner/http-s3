b70dd90ef08438f996628f206ed994a1099823eb
Rebecca Skinner <rebecca.skinner@ctl.io>
2016-05-13 08:53:59 -0500

    wip - signature creation

--- src/Network/HTTP/S3/Signature.hs
+++ src/Network/HTTP/S3/Signature.hs
@@ -1,8 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.S3.Signature where
 import qualified Network.HTTP as HTTP
+import Data.Time.Clock
+import Data.Time.Format
+import qualified Data.ByteString.Char8 as B
+import Crypto.Hash
+import Crypto.MAC.HMAC
+import Data.List
 
-import Data.Text
+data SigningRecord = SigningRecord { sigMethod :: HTTP.RequestMethod
+                                   , sigContent :: B.ByteString
+                                   , sigContentType :: B.ByteString
+                                   , sigDate :: UTCTime
+                                   , sigResource :: Request B.ByteString
+                                   }
 
-signRequest :: String -> String -> HTTP.Request a -> Maybe (HTTP.Request a)
-signRequest access secret request = Nothing
+data Request a = HostStyleRequest a | PathStyleRequest a
+
+md5sum :: B.ByteString -> B.ByteString
+md5sum = B.pack . show . (hash :: B.ByteString -> Digest MD5)
+
+md5Str :: String -> B.ByteString
+md5Str = md5sum . B.pack
+
+rfc1123DateString :: String
+rfc1123DateString = "%a, %d %b %Y %H:%M:%S %Z"
+
+httpDateFormat :: FormatTime t => t -> B.ByteString
+httpDateFormat = B.pack . formatTime defaultTimeLocale rfc1123DateString
+
+{- For host style requests this assumes that everything before the first '.'
+   is the bucket name, and everything after it until the first '/' or end of
+   the string is the hostname -}
+canonizeRequestString :: Request B.ByteString -> B.ByteString
+canonizeRequestString req =
+    let (PathStyleRequest req') = toPathStyle req in req'
+    where
+      toPathStyle pth@(PathStyleRequest _) = pth
+      toPathStyle (HostStyleRequest hostStyle) =
+          let (bucket, host') = B.break (=='.') hostStyle
+              host = if B.null host' then B.empty else B.tail host'
+          in PathStyleRequest hostStyle
+
+
+hashSigningRequest :: SigningRecord -> B.ByteString
+hashSigningRequest sigRecord =
+    B.intercalate (B.pack "\n") [ (B.pack . show) (sigMethod sigRecord)
+                                  , md5sum (sigContent sigRecord)
+                                  , sigContentType sigRecord
+                                  , (httpDateFormat . sigDate) sigRecord
+                                  , (canonizeRequestString . sigResource) sigRecord
+                                  ]
+
+mkSigningRequest req = SigningRecord { sigMethod = rqMethod req
+                                     , sigContent = (md5Sum . rqBody) req
+                                     ,
+
+signRequest :: B.ByteString -> B.ByteString -> HTTP.Request B.ByteString -> Maybe (HTTP.Request B.ByteString)
+signRequest access secret request =
+    let requestSig = hashSigningRequest $ mkSigningRequest request
+        signedReq = hmac secret requestSig :: Digest SHA1
+    in Nothing --(pack "AWS)
--- test/Network/HTTP/S3/SignatureSpec.hs
+++ test/Network/HTTP/S3/SignatureSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.S3.SignatureSpec (main, spec) where
 
 import Test.Hspec
@@ -6,7 +7,6 @@ import Test.QuickCheck
 import Network.HTTP.S3.Signature
 import qualified Network.HTTP as HTTP
 import Network.URI
-import Data.Text as T
 import Data.Maybe
 import Control.Monad
 
@@ -16,11 +16,17 @@ main = hspec spec
 instance (Show a) => Eq (HTTP.Request a) where
   a == b = (show a) == (show b)
 
+fakeEndpoint :: String
+fakeEndpoint = "http://fakes3-endpoint.test"
+
 fakeEndpointURI :: URI
 fakeEndpointURI = fromJust $ parseURI "http://fakes3-endpoint.test/admin"
 
-fakeBadGetRequest :: HTTP.Request T.Text
-fakeBadGetRequest = HTTP.Request fakeEndpointURI HTTP.GET [] empty
+fakeBadGetRequest :: HTTP.Request String
+fakeBadGetRequest = HTTP.Request fakeEndpointURI HTTP.GET [] ""
+
+fakeGetRequest :: HTTP.Request String
+fakeGetRequest = HTTP.getRequest fakeEndpoint
 
 fakeAccess :: String
 fakeAccess = "fakeaccess1"
@@ -34,3 +40,8 @@ spec = do
     context "When missing headers" $ do
       it "Returns 'Nothing' when missing a date header" $ do
         (signRequest fakeAccess fakeSecret fakeBadGetRequest) `shouldBe` Nothing
+
+    context "When given valid input data" $ do
+      it "Returns 'Just (Request Text)'" $ do
+        pendingWith "Requires fuller implementation"
+
